cfcc52b6345d46a5ef0450a60ec2d02fc9580e54
shubham <shuubhamsharma98@gmail.com>
2025-01-30 12:01:09 -0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg"
+          alt="Logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/RestaurantCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const RestaurantCard = (props) => {
+  const{resData}= props
+
+  const {name, avgRating,cuisines, costForTwo, deliveryTime,} = resData?.data;
+  return (
+    <div className="res-card">
+      <img
+        className='res-logo'
+        src="https://res.cloudinary.com/hz3gmuqw6/image/upload/c_fill,h_450,q_auto,w_710/f_auto/food-trends-2021-phprMLwAP"
+        alt="Restaurant"
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(",")}</h4>
+      <h4>{costForTwo/100}</h4>
+      <p>⭐ 4.5 • 30 mins</p>
+    </div>
+  );
+};
+
+export default RestaurantCard;
